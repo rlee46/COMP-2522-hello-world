ad0a75dbae54b2cc3b294772d396a8df8627602a
rlee46 <rylee2012@gmail.com>
2022-09-06 09:35:08 -0700

    Fixed checkstyle errors

--- src/main/java/org/comp2522/FizzBuzzProcessor.java
+++
@@ -1,7 +0,0 @@
-package org.comp2522;
-
-public class FizzBuzzProcessor {
-    public static void main(String[] args) {
-        System.out.println("my favourite football team is Seattle");
-    }
-}
---
+++ src/main/java/org/comp2522/Onboarding.java
@@ -0,0 +1,13 @@
+package org.comp2522;
+
+/**
+* This class is sample code for onboarding tutorial.
+*/
+public class Onboarding {
+  /**
+  * This method prints a unique greeting.
+  */
+  public static void main(String[] args) {
+    System.out.println("my favourite football team is Seattle");
+  }
+}
